ccbf166e229dba0348620b03bc468da094f9edfd
Volune <jeremy@volune.net>
2016-06-04 05:12:13 +0200

    iterator::compact

---
+++ test/iterator/compact.js
@@ -0,0 +1,27 @@
+import expect from 'must';
+import compact from '../../lib/iterator/compact';
+import {
+  shouldReturnIterable,
+} from '../helpers';
+
+describe('filter', () => {
+  compact::shouldReturnIterable();
+
+  it('returns expected values [A,B]', () => {
+    const array = ['A', null, 'B'];
+    const result = array::compact();
+    expect([...result]).to.eql(['A', 'B']);
+  });
+
+  it('returns expected no values from undefined, null, 0, "", NaN, false', () => {
+    const array = [undefined, null, 0, '', NaN, false, 1];
+    const result = array::compact();
+    expect([...result]).to.eql([1]);
+  });
+
+  it('returns no values from empty source', () => {
+    const array = [];
+    const result = array::compact();
+    expect([...result]).to.eql([]);
+  });
+});

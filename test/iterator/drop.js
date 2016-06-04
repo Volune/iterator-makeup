247d3e6de03001a5b8785ad577c527d86d364ac5
Volune <jeremy@volune.net>
2016-06-04 05:33:09 +0200

    iterator::drop

---
+++ test/iterator/drop.js
@@ -0,0 +1,33 @@
+import expect from 'must';
+import drop from '../../lib/iterator/drop';
+import {
+  shouldReturnIterable,
+} from '../helpers';
+
+describe('filter', () => {
+  drop::shouldReturnIterable();
+
+  it('returns all values except first', () => {
+    const array = ['A', 'B', 'C'];
+    const result = array::drop();
+    expect([...result]).to.eql(array.slice(1));
+  });
+
+  it('returns all values when drop nothing', () => {
+    const array = ['A', 'B', 'C'];
+    const result = array::drop(0);
+    expect([...result]).to.eql(array);
+  });
+
+  it('returns no values when drop too much', () => {
+    const array = ['A', 'B', 'C'];
+    const result = array::drop(array.length + 1);
+    expect([...result]).to.eql([]);
+  });
+
+  it('returns no values from empty source', () => {
+    const array = [];
+    const result = array::drop();
+    expect([...result]).to.eql([]);
+  });
+});

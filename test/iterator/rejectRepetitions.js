f57a47eb31457d4fcd64851f53a47013417906cd
Volune <jeremy@volune.net>
2016-06-04 06:54:00 +0200

    iterator::rejectRepetitions

---
+++ test/iterator/rejectRepetitions.js
@@ -0,0 +1,33 @@
+import expect from 'must';
+import rejectRepetitions from '../../lib/iterator/rejectRepetitions';
+import {
+  shouldReturnIterable,
+} from '../helpers';
+
+describe('rejectRepetitions', () => {
+  rejectRepetitions::shouldReturnIterable();
+
+  it('returns expected from [1,2,3]', () => {
+    const array = [1, 2, 3];
+    const result = array::rejectRepetitions();
+    expect([...result]).to.eql([1, 2, 3]);
+  });
+
+  it('returns expected from [1,2,2,3,3,3]', () => {
+    const array = [1, 2, 2, 3, 3, 3];
+    const result = array::rejectRepetitions();
+    expect([...result]).to.eql([1, 2, 3]);
+  });
+
+  it('returns expected from [undefined, undefined]', () => {
+    const array = [undefined, undefined];
+    const result = array::rejectRepetitions();
+    expect([...result]).to.eql([undefined]);
+  });
+
+  it('returns no values from empty source', () => {
+    const array = [];
+    const result = array::rejectRepetitions();
+    expect([...result]).to.eql([]);
+  });
+});
